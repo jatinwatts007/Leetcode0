f2c6a1dc8fee6e62379bc79264423e99e0567f85
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2021-08-20 21:54:00 +0530

    Create merge between LL.cpp

---
+++ merge between LL.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* temp1 = list1;
+        ListNode* temp2;
+        for(int i=0;i<a-1;i++)
+        {
+            temp1 = temp1->next;
+        }
+        temp2 = temp1->next;
+        temp1->next=list2;
+        while(temp1->next!=NULL) temp1=temp1->next;
+        for(int i=0;i<b-a+1;i++)
+        {
+            temp2=temp2->next;
+        }
+        temp1->next = temp2;
+        return list1;
+        
+    }
+};
