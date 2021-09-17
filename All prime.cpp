9c95957a0704466dcedeff534b811bf4c93b8e07
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2021-09-17 23:26:12 +0530

    Create All prime.cpp

---
+++ All prime.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc+=.h>
+using namespace std;
+int is_prime[1000001];
+int main(){
+  int max = 1000000;
+  for(int i=0;i<=max;i++) is_prime[i]=0;
+  is_prime[0]=is_prime[1]=0;
+  for(int i=2;i<=max;i++){
+    if(is_prime[i]{
+      cout<<is_prime[i]<<" ";     
+      for(int j=i*i;j<=max;j+=i) is_prime[j]=0;
+    }
+  }
+}
