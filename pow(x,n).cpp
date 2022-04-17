cce869be0c966eb68e497263dbc62b23c4b5b584
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2022-04-17 23:37:14 +0530

    Create pow(x,n).cpp

---
+++ pow(x,n).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        if(n<0){
+            x=1/x;
+        }
+        while(n){
+            if(n&1)
+                ans*=x;
+            x*=x;
+            n/=2;
+        }
+        return ans;
+    }
+};
